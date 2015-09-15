8ccffc6e13070ff9a21ea7fb55c28b88b98cdabd
mdpulis <pulis.michael@gmail.com>
2015-09-15 18:58:16 -0400

    Town and some small edits
    Added town, altered some scripts, and changed UI on Dungeon scene to
    scale with screen at 1920x1080

---
+++ DungeonRunner/Assets/Scripts/Town/TownAreaLoader.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TownAreaLoader : MonoBehaviour {
+
+	public GameObject mainCamera;
+	public GameObject eventSystem;
+	public Text areaDebugText;
+
+	//Load the town buildings
+	public void LoadArea(string areaSceneName){
+		areaDebugText.text = "You have selected area: " + areaSceneName;
+		//NOTE: Until we have real scenes to go to, this will cause double cameras and event systems due to DontDestroyOnLoad
+		Application.LoadLevel (areaSceneName);
+		DontDestroyOnLoad (mainCamera);
+		//It might be better to destroy our event system? It affects wasd/controller UI inputs
+		DontDestroyOnLoad (eventSystem);
+	}
+
+	//Start the dungeon sequence
+	public void GoToDungeon(string dungeonSceneName){
+		Application.LoadLevel (dungeonSceneName);
+	}
+}
