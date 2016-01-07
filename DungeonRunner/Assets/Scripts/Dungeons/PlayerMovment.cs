44b359f700a7d68b786b6ba343a39981d026fded
pixelpeeper <pixelpeeper@users.noreply.github.com>
2016-01-06 20:03:00 -0500

    cleaned up player movement, and added a ceiling and object collision detection. currently not used for anything.

--- DungeonRunner/Assets/Scripts/Dungeons/PlayerMovment.cs
+++ DungeonRunner/Assets/Scripts/Dungeons/PlayerMovment.cs
@@ -7,59 +7,75 @@ using UnityStandardAssets.CrossPlatformInput;
 public class PlayerMovment : MonoBehaviour {
 	
 	public 		bool 		alowMove = true;		// tracks X movment permissions
-	public		bool		AlowAirAceleration = true;	// permits the player to acelerate on asecnt of jumps
+	public		bool		alowAirAceleration = true;	// permits the player to acelerate on asecnt of jumps
 	public 		bool 		hasDoubleJumped = false;// tracks if player has double jumped
 	public 		bool 		jumpBuffer = false;		// tracks jump comands, useed for buffering when jumped
 	public 		float 		maxSpeed = 10f;			// max speed the player can travel
 	public 		float 		jumpForce = 200f;		// vertical force when jumping
 	public		bool		stopPlayer = false;		// prevents input but alows physics
 	public		bool		recentDamage = false;	// tracks if the damage thigns been triggered
-	public		bool		AlowAirAcelerationWhenDamage = false; // prevents air aceleration from interupting damage feedback
+	public		bool		alowAirAcelerationWhenDamage = false; // prevents air aceleration from interupting damage feedback
 	private		float		deAceleration = 10f;	// slows player movments, only use for gradent aceleration.
+	public		float		deAcelerationDamage = 20f;	// varable sets how much to deacelerate by.
+	public		float		deAcelerationStoped = 10f;	// varable sets how much to deacelerate by.
 	
+	[SerializeField] private 	LayerMask 	whatIsCeiling;			// A mask determining what is the ceiling to the character
 	[SerializeField] private 	LayerMask 	whatIsGround;			// A mask determining what is ground to the character
+	[SerializeField] private 	LayerMask 	whatIsObstacle;			// A mask determining what is an obstacle to the character
 	const 		float 		groundedRadius = .2f;	// Radius of the overlap circle to determine if grounded
+	const 		float 		forwardRadius = .2f;	// Radius of the overlap circle to determine if coliding with anythign in front
 	private 	Transform 	ceilingCheck;   		// A position marking where to check for ceilings
 	private 	Transform 	groundCheck;			// A position marking where to check if the player is grounded.
+	private 	Transform 	forwardCheck;			// A position marking where to check if the player is coliding with anything.
 	private 	bool 		grounded; 				// Whether or not the player is grounded.
 	private 	Animator 	animate;            	// Reference to the player's animator component.
 	private 	Rigidbody2D m_Rigidbody2D;			// references the player object
 	
-	
-
-	private bool m_Jump;	
-	
-	
-	
-	
+	private bool m_Jump;
 	
 	private void Awake() {
 		// Setting up references.
 		groundCheck = transform.Find("GroundCheck");
 		ceilingCheck = transform.Find("CeilingCheck");
+		forwardCheck = transform.Find("ForwardCheck");
 		animate = GetComponent<Animator>();
 		m_Rigidbody2D = GetComponent<Rigidbody2D>();
 	}
 	
 	//handels ground checks on update
 	private void FixedUpdate() {
-		//Read the inputs.
 		//bool crouch = Input.GetKey(KeyCode.LeftControl);
-		//float h = CrossPlatformInputManager.GetAxis("Horizontal");
-		// Pass all parameters to the character control script.
-		//m_Character.Move(h, crouch, m_Jump);
 		
 		grounded = false;
 		
 		//check if the player is grounded using a circlecast to ground. if it hits, it sticks
 		Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
 		for (int i = 0; i < colliders.Length; i++) {
-			if (colliders[i].gameObject != gameObject)
+			if (colliders[i].gameObject != gameObject) {
 				grounded = true;
-				if (!recentDamage) {AlowAirAcelerationWhenDamage = true;}
+				if (!recentDamage) {alowAirAcelerationWhenDamage = true;}
+			}
 		}
-		Move (1f, false, m_Jump, false, false);
+		//check if the player is hitting the ceiling using a circlecast to ceiling.
+		Collider2D[] collidersCeiling = Physics2D.OverlapCircleAll(ceilingCheck.position, groundedRadius, whatIsGround);
+		for (int i = 0; i < collidersCeiling.Length; i++) {
+			if (collidersCeiling[i].gameObject != gameObject) {
+				Debug.Log("Hit the ceiling");
+				//grounded = true;
+				if (!recentDamage) {alowAirAcelerationWhenDamage = true;}
+			}
+		}
+		//check if the player is hitting an obstical using a circlecast to obstacle.
+		Collider2D[] collidersForward = Physics2D.OverlapCircleAll(forwardCheck.position, forwardRadius, whatIsObstacle);
+		for (int i = 0; i < collidersForward.Length; i++) {
+			if (collidersForward[i].gameObject != gameObject) {
+				Debug.Log("we colided!");
+				//grounded = true;
+				//if (!recentDamage) {alowAirAcelerationWhenDamage = true;}
+			}
+		}
 		
+		Move (1f, false, m_Jump, false, false);
 		animate.SetBool("Ground", grounded);
 		animate.SetFloat("vSpeed", m_Rigidbody2D.velocity.y);
 	}
@@ -79,14 +95,17 @@ public class PlayerMovment : MonoBehaviour {
 	}
 	
 	public void Notmoving() {
-		deAceleration = 10f;
+		deAcelerationStoped = 10f;
 		animate.SetBool("standing", grounded);
+		animate.SetFloat("Speed", Mathf.Abs(0f));
 	}
 	
 	public void Move(float move, bool crouch, bool jump, bool sprint, bool damage) {
 		if (!stopPlayer) {  //prevents player movment, may be usefull?
 			
 //------------------------------------//!!!SETS FORWARD MOVMENT!!!//-----------------------------------------------------
+			
+			
 			//reduces speed if crouching
 			move = (crouch ? move*0.5f : move);
 			//only sets the characters forward vector when grounded or air aceleration is permitted
@@ -99,13 +118,11 @@ public class PlayerMovment : MonoBehaviour {
 					Debug.Log("acelerating-"+deAceleration);
 				}
 				
-				
-				
 				// The Speed animator parameter is set to the absolute value of the horizontal input.
 				animate.SetFloat("Speed", Mathf.Abs(move));
 				//sets the forward velocity
 				m_Rigidbody2D.velocity = new Vector2(move*maxSpeed/deAceleration, m_Rigidbody2D.velocity.y);
-			} else if (AlowAirAcelerationWhenDamage && AlowAirAceleration) {
+			} else if (alowAirAcelerationWhenDamage && alowAirAceleration) {
 				if (recentDamage) {Debug.Log("!!! AIR");}
 				animate.SetFloat("Speed", Mathf.Abs(move));
 				
@@ -145,7 +162,7 @@ public class PlayerMovment : MonoBehaviour {
 				//invokes a temporary state in which hte character will jump at the next posible chance. times out after 1/8th of a second
 				Debug.Log("jump3");
 				jumpBuffer = true;
-				Invoke("clearJumpBuffer", 0.125f);
+				Invoke("ClearJumpBuffer", 0.125f);
 			}
 			m_Jump = false;
 			
@@ -155,31 +172,24 @@ public class PlayerMovment : MonoBehaviour {
 			if (damage) {
 				recentDamage = true;
 				grounded = false;
-				AlowAirAcelerationWhenDamage = false;
+				alowAirAcelerationWhenDamage = false;
 				animate.SetBool("Ground", false);
 				//animate.SetBool("Damage", true);
 				m_Rigidbody2D.velocity = new Vector2(((move*maxSpeed) / -1f), jumpForce);
-				deAceleration = 10f;
-				//InvokeRepeating("deAcelerationdecreas", 1f, 0.25f);
-				Invoke("clearDamage", 0.5f);
+				deAceleration = deAcelerationDamage;
+				Invoke("ClearDamage", 0.5f);
 			}
 		}
 	}
 	
 	//clears the jump buffer
-	public void clearJumpBuffer() {
+	public void ClearJumpBuffer() {
 		jumpBuffer = false;
 		Debug.Log("buffered jump spent");
 	}
 	//clears the recent damage value
-	public void clearDamage() {
+	public void ClearDamage() {
 		recentDamage = false;
 		Debug.Log("Damage jump cleared");
 	}
-	public void deAcelerationdecreas() {
-		deAceleration = deAceleration + 0.5f;
-		Debug.Log("acelerating");
-	}
-	
-	
 }
